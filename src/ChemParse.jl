1da7c63e25c7afcf5ccceb1c67a5cd7ac2058877
C. Brenhin Keller <cbkeller@dartmouth.edu>
2026-05-31 17:35:36 -0400

    Initial implementation

--- src/ChemParse.jl
+++ src/ChemParse.jl
@@ -1,5 +1,114 @@
 module ChemParse
 
-# Write your package code here.
+    # Add and multiply chemical formulae as dictionaries
+    function add!(x::Dict, y::Dict)
+        for k in keys(y)
+            if k in keys(x)
+                x[k] += y[k]
+            else
+                x[k] = y[k]
+            end
+        end
+        return x
+    end
+    function add!(x::Dict, p::Pair)
+        if p.first in keys(x)
+            x[p.first] += p.second
+        else
+            x[p.first] = p.second
+        end
+        return x
+    end
+    function multiply!(x::Dict, n::Number)
+        for k in keys(x)
+            x[k] *= n
+        end
+        return x
+    end
 
-end
+    # Input cleaning
+    remove_whitespace(fstr) = replace(fstr, r"\s+"=>"")
+    remove_ions(fstr::AbstractString) = replace(fstr, r"([A-Z][a-z]*)[0-9]*[\+\-]+"=>s"\1")
+    function format_vacancies(fstr::AbstractString)
+        fstr = replace(fstr, "[box]"=>"□")
+        fstr = replace(fstr, "[]"=>"□")
+        fstr = replace(fstr, "◻"=>"□")
+    end
+    function sanitize(fstr::AbstractString)
+        # Require matching parentheses and brackets
+        @assert count(isequal('('), fstr) == count(isequal(')'), fstr) "Unmatched parentheses in formula $fstr"
+        @assert count(isequal('['), fstr) == count(isequal(']'), fstr) "Unmatched square brackets in formula $fstr"
+
+        # Remove whitespace, if any
+        if contains(fstr, ' ') || contains(fstr, '\t')
+            fstr = remove_whitespace(fstr)
+        end
+        # Remove ions, if any
+        if contains(fstr, '+') || contains(fstr, '-')
+            fstr = remove_ions(fstr)
+        end
+
+        return fstr
+    end
+
+    # Parsing
+    function parse_formula(fstr::AbstractString; include_vacancies::Bool=false)
+        # Clean up inputs
+        fstr = sanitize(fstr)
+        # Deal with vacancies
+        fstr = if include_vacancies
+            format_vacancies(fstr)
+        else
+            replace(fstr, "□"=>"◻")
+        end
+        
+        # Parse
+        return formula(fstr)
+    end
+
+    # Recursive parsing for all other conditions
+    function formula(fstr::AbstractString)
+        if contains(fstr, '·') || contains(fstr, '⋅')
+            # Deal with any bound complexes
+            m = match(r"(?<prefix>^.*)[·⋅]+(?<number>[0-9\.]*)(?<suffix>.*$)", fstr)
+            number = isempty(m["number"]) ? 1.0 : parse(Float64, m["number"])
+
+            f = multiply!(formula(m["suffix"]), number)
+            isempty(m["prefix"]) || add!(f, formula(m["prefix"]))
+            return  f
+
+        elseif contains(fstr, '[') && contains(fstr, ']')
+            # Separate out groups in square brackets, requiring balanced brackets for matched group
+            m = match(r"(?<prefix>[^\[]*)\[(?<group>[^\]\[]*(?:\[^\]\[]*(?:\[[^\]\[]*(?:\[[^\]\[]*\][^\]\[]*)*\][^\]\[]*)*\][^\]\[]*)*)\](?<number>[0-9\.]*)(?<suffix>.*$)", fstr)
+            number = isempty(m["number"]) ? 1.0 : parse(Float64, m["number"])
+
+            f = multiply!(formula(m["group"]), number)
+            isempty(m["prefix"]) || add!(f, formula(m["prefix"]))
+            isempty(m["suffix"]) || add!(f, formula(m["suffix"]))
+            return f
+
+        elseif contains(fstr, '(') && contains(fstr, ')')
+            # Separate out groups in parentheses, requiring balanced parentheses for matched group
+            m = match(r"(?<prefix>^[^(]*)\((?<group>[^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*)\)(?<number>[0-9\.]*)(?<suffix>.*$)", fstr)
+            number = isempty(m["number"]) ? 1.0 : parse(Float64, m["number"])
+            if contains(m["group"], ',') && !contains(m["group"], '(')
+                number /= (count(c->c==',', (m["group"])) + 1) 
+            end
+            
+            f = multiply!(formula(m["group"]), number)
+            isempty(m["prefix"]) || add!(f, formula(m["prefix"]))
+            isempty(m["suffix"]) || add!(f, formula(m["suffix"]))
+            return f
+        else
+            # Parse through each posible element
+            f = Dict{Symbol,Float64}()
+            for m in eachmatch(r"(?<element>[□A-Z][a-z]?)(?<number>[0-9\.]*)", fstr)
+                number = isempty(m["number"]) ? 1.0 : parse(Float64, m["number"])
+                add!(f, Symbol(m["element"]) => number)
+            end
+            return f
+        end
+    end
+
+    export parse_formula
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,22 @@ using ChemParse
 using Test
 
 @testset "ChemParse.jl" begin
-    # Write your tests here.
+    # Some example minerals and inorganic compounds
+    @test parse_formula("SiO2") == Dict(:Si => 1.0, :O => 2.0) # Quartz
+    @test parse_formula("CuSO4·5H2O") == Dict(:H => 10.0, :S => 1.0, :Cu => 1.0, :O => 9.0) # Chalcanthite
+    @test parse_formula("(Fe,Mg)2SiO4") == Dict(:Fe => 1.0, :Mg => 1.0, :Si => 1.0, :O => 4.0) # Olivine
+    @test parse_formula("(Ca,Na)(Mg,Fe,Al,Ti)(Si,Al)2O6") == Dict(:Al => 1.25, :Fe => 0.25, :Ti => 0.25, :Mg => 0.25, :Ca => 0.5, :Na => 0.5, :Si => 1.0, :O => 6.0) # Augite
+    @test parse_formula("(Ca,Na)2−3(Mg,Fe,Al)5(Al,Si)8O22(OH,F)2") == Dict(:Al => 5.666666666666667, :Fe => 1.6666666666666667, :F => 1.0, :H => 1.0, :Mg => 1.6666666666666667, :Ca => 1.0, :Na => 1.0, :Si => 4.0, :O => 23.0) # Hornblende
+    @test parse_formula("(Na,K,Ca)24(Na,Ca)4Ca4(Si,Al)48O96(SO4)4(SO3,CO3)2(OH,Cl)2(H2O,OH)4") == Dict(:C => 1.0, :Cl => 1.0, :Al => 24.0, :H => 7.0, :K => 8.0, :S => 5.0, :Ca => 14.0, :Na => 10.0, :O => 123.0, :Si => 24.0)
+    @test parse_formula("Ca11(Ce,H2O,Ca)8Mg(Al,Fe)4(Mg,Ti,Fe3+)8[Si2O7]4[(SiO4)8(H4O4)2](OH)9")
+
+    # Some example organic compounds
+    @test parse_formula("CH3COOH") == Dict(:H => 4.0, :O => 2.0, :C => 2.0) # Acetic acid
+    @test parse_formula("C5H6N2O2") == Dict(:N => 2.0, :H => 6.0, :O => 2.0, :C => 5.0) # Thymine
+    @test parse_formula("C11H12N2O2") == Dict(:N => 2.0, :H => 12.0, :O => 2.0, :C => 11.0) # Tryptophan
+
+    # Some example coordination compounds
+    @test parse_formula("[Ru((C5H4N)2)3]Cl2·6H2O") == Dict(:Cl => 2.0, :N => 6.0, :Ru => 1.0, :H => 36.0, :O => 6.0, :C => 30.0) # Tris(bipyridine)ruthenium(II) chloride
+    @test parse_formula("[(Co(OH2)5)2]Cl4") == Dict(:Cl => 4.0, :H => 20.0, :Co => 2.0, :O => 10.0)
+
 end
